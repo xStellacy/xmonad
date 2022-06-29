61697650e986cd4a1d6253272bcf53681a697c9b
Stellacy <stellacy@tutanota.com>
2022-06-29 06:43:41 -0700

    hello

---
+++ lib/Colors/TokyoNight.hs
@@ -0,0 +1,28 @@
+module Colors.TokyoNight where
+
+import XMonad
+
+colorScheme = "tokyo-night"
+
+colorBack = "#1a1b26"
+colorFore = "#a9b1d6"
+
+color01 = "#414868"
+color02 = "#f7768e"
+color03 = "#73daca"
+color04 = "#e0af68"
+color05 = "#7aa2f7"
+color06 = "#bb9af7"
+color07 = "#7dcfff"
+color08 = "#a9b1d6"
+color09 = "#0f0f14"
+color10 = "#8c4351"
+color11 = "#33635c"
+color12 = "#8f5e15"
+color13 = "#34548a"
+color14 = "#5a4a78"
+color15 = "#0f4b6e"
+color16 = "#343b58"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x1a1b26"
